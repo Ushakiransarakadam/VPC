aae42e4457a555237ae39c177995334668236f20
Usha Kiran Sarakadam <121487011+Ushakiransarakadam@users.noreply.github.com>
2023-02-20 18:56:54 +0000

    Create vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "4.55.0"
+    }
+  }
+}
+
+provider "aws" {
+    profile = "default"
+    region = "eu-west-2"
+  
+}
